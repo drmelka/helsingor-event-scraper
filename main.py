fba540327c1d9b1b002717ca1eeb4179b571858b
drmelka <drmelka@hotmail.com>
2025-05-14 12:02:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from scrapers import kuto
+import scrapers.kuto as kuto
 from sheets import append_rows
 
 def run():
     rows = list(kuto.parse())
+    print(f"Found {len(rows)} events")        # <- DEBUG
     append_rows(rows)
+    print("Rows written to sheet")            # <- DEBUG
 
 if __name__ == "__main__":
     run()
